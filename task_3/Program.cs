27f6883fbc3a83b478120f62e3fb6b23c9b0a464
Руслан Завьялов <ruslan70797@gmail.com>
2022-07-11 23:54:49 +0300

    Задание 3

---
+++ task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Введенное число является четным");
+}
+else
+{
+    Console.WriteLine("Введеное число является не четным");
+}
