ec7a9d5f3dc21d7bc4635ee71c7b5836726afdc2
Руслан Завьялов <ruslan70797@gmail.com>
2022-07-11 13:15:15 +0300

    Задание 1

---
+++ task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿double a, b;
+Console.Write("Введите первое число: ");
+a = Double.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = Double.Parse(Console.ReadLine());
+if (a > b)
+    Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
+else Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
