cd32a8824ac07793a7b05f34f4cc457ff0936637
Руслан Завьялов <ruslan70797@gmail.com>
2022-07-11 23:30:24 +0300

    Задание 2

---
+++ task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+ int max = a;
+if (b > max)
+ {
+    max = b;
+ }
+ if (c > max)
+ {
+    max = c;
+ }
+ Console.Write("Максимальное число: ");
+ Console.Write(max);
